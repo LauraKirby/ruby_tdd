aa256e49d1143b89f60f6ab5b83da08b8dc74d09
LauraKirby <laurakirby26@gmail.com>
2015-07-07 22:55:43 -0700

    fails test for class variable called favorite_treats which is an array of treats

--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -3,11 +3,17 @@ require_relative '../dog.rb'
 
 describe "Dog" do 
 	let (:rusty){Dog.new("kacey", 3)}
-		describe "instance of Dog" do 
+		
+	describe "instance of Dog" do 
 		it "should inherit from Animal" do
 			expect(rusty.sleeping).to be(true)
 		end
 	end 
 	
+	describe "class variable favorite_treats" do
+		it "should be an array of treats" do 
+			expect(rust.favorite_treats).to be(["bacon", "liver"]) 
+		end 
+	end 
 
 end
