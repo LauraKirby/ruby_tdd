f1c47533c5b818f2e88a9d4c6d2c0fcf0507e0e5
LauraKirby <laurakirby26@gmail.com>
2015-07-07 22:11:11 -0700

    wake_up method passes

--- animal.rb
+++ animal.rb
@@ -7,13 +7,18 @@ class Animal
 		@sleeping = sleeping
 	end 
 
+	def wake_up 
+		@sleeping = false
+	end 
+
+
 end # end class 
 
-laura = Animal.new("laura", 28, false)
+# laura = Animal.new("laura", 28,)
 
-#QUESTION
-p laura #it's weird that you can view the entire object without getter methods, 
-# however if you use laura.name - you do not have access
+# #QUESTION
+# p laura #it's weird that you can view the entire object without getter methods, 
+# # however if you use laura.name - you do not have access
 
-p laura.name
+# p laura.name
 
